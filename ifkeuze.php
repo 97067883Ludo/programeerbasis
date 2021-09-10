ada528297245c5b7d5cc43ded3cf5b01bee3f982
97067883Ludo <97067883@st.deltion.nl>
2021-09-10 13:09:11 +0200

    added code to ifkeuze.php

--- ifkeuze.php
+++ ifkeuze.php
@@ -1,16 +1,17 @@
 <?php
 $naam = readline("geef je naam op ");
 $leeftijd = readline("en jouw leeftijd is? ");
+$leeftijdifstate = 22;
 
-if($leeftijd < 22){
+if($leeftijd < $leeftijdifstate){
     echo "hallo $naam jij bent jonger dan 22 namelijk: $leeftijd";
 
 }
-if($leeftijd > 22){
+if($leeftijd > $leeftijdifstate){
     echo "hallo $naam jij bent ouder dan 22 namelijk $leeftijd";
 
 }
-if($leeftijd == 22){
+if($leeftijd == $leeftijdifstate){
     echo "hee, $naam jij bent toevallig precies 22 jaar oud";
 
 }
