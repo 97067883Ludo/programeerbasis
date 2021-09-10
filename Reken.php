1fb09121a2ea3cc888d0d6d6f5b47aa2f3cec24b
97067883Ludo <97067883@st.deltion.nl>
2021-09-10 09:11:43 +0200

    added reken.php

---
+++ Reken.php
@@ -0,0 +1,25 @@
+<?php
+
+$getal1 = 15;
+$getal2 = 5;
+
+$som=$getal1 + $getal2;
+echo "$getal1 + $getal2 = ".$som;
+echo "\n";
+
+$som=$getal1 - $getal2;
+echo "$getal1 - $getal2 = ".$som;
+echo "\n";
+
+$som=$getal1 * $getal2;
+echo "$getal1 x $getal2 = ".$som;
+echo "\n";
+
+$som=$getal1 / $getal2;
+echo "$getal1 / $getal2 = ".$som;
+echo "\n";
+
+$rest=$getal1 % $getal2;
+echo "$getal1 % $getal2 = ".$rest;
+echo "\n";
+?>
